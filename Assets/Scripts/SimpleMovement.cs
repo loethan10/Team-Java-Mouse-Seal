a4669a39701957e74344f399a35339944caf9b90
Ethan Lo <loethan101@gmail.com>
2024-10-14 19:46:12 -0700

    Start/end screen, sprite, ending
    Added start and end screens (and a way to start and end the game); also added sprite flipping movement

--- Assets/Scripts/SimpleMovement.cs
+++ Assets/Scripts/SimpleMovement.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class SimpleMovement : MonoBehaviour
 {
-    public float speed;
+    public float speed = 7;
     private float move;
     private float uppies;
+    
     private Rigidbody2D rb;
+
+    bool facingRight = true;
 
     // Start is called before the first frame update
     void Start()
@@ -24,5 +27,21 @@ public class SimpleMovement : MonoBehaviour
 
         //rb.velocity = new Vector2(move * speed, rb.velocity.y);
         //rb.velocity = new Vector2(uppies * speed, rb.velocity.x);
+
+        if (move > 0 && !facingRight){
+            Flip();
+        }
+
+        if (move < 0 && facingRight){
+            Flip();
+        }
+    }
+
+    void Flip(){
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        facingRight = !facingRight;
     }
 }
